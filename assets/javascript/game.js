737973aec971068ec49c4b32f20f5dbfdff8fb68
Trevor <tstike44@hotmail.com>
2019-10-09 13:17:01 -0700

    wins are working

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,127 +3,129 @@
 //generate random number between (19-120)
 
 //crystals can be clicked with a value
-    //crystal value is random between (1-12)
+//crystal value is random between (1-12)
 
 //score = crystal clicked value added
 
 //win/loss contingency
-    //if the crystal added value = random number then user wins
-    //if the crystal value is higher than the random number then the   user loses
+//if the crystal added value = random number then user wins
+//if the crystal value is higher than the random number then the   user loses
 
 
 
-    
+//globals
 var wins = 0;
 var losses = 0;
 var score = 0;
-var gem1 = Math.floor(Math.random()*12) + 1;
-var gem2 = Math.floor(Math.random()*12) + 1;
-var gem3 = Math.floor(Math.random()*12) + 1;
-var gem4 = Math.floor(Math.random()*12) + 1; 
-var randomNumber = Math.floor(Math.random()*120) + 19;
+var gem1 = Math.floor(Math.random() * 12) + 1;
+var gem2 = Math.floor(Math.random() * 12) + 1;
+var gem3 = Math.floor(Math.random() * 12) + 1;
+var gem4 = Math.floor(Math.random() * 12) + 1;
+var randomNumber = $("#randomNum");
+randomNumber = Math.floor(Math.random() * 120) + 19;
 
 
-$(document).ready(function() {
-    
-function reset() {
-    
-randomNumber = Math.floor(Math.random()*120) + 19;
-$('#randomNum').html(randomNumber);
+$(document).ready(function () {
+	//function to reset the scores
+	function reset() {
+
+		randomNumber = Math.floor(Math.random() * 120) + 19;
+		$('#randomNum').html(randomNumber);
+		score = 0;
+
+		gem1 = Math.floor(Math.random() * 12) + 1;
+		gem2 = Math.floor(Math.random() * 12) + 1;
+		gem3 = Math.floor(Math.random() * 12) + 1;
+		gem4 = Math.floor(Math.random() * 12) + 1;
+		$('#score').html(score)
+	}
 
 
-score = 0;
+	//not generating new number on reset
+	//not adding wins/losses
+	//
+	$('#gem1').click(function () {
+		console.log('gem1: ' + gem1);
+		var totalScore = score += gem1;
 
- gem1 = Math.floor(Math.random()*12) + 1;
- gem2 = Math.floor(Math.random()*12) + 1;
- gem3 = Math.floor(Math.random()*12) + 1;
- gem4 = Math.floor(Math.random()*12) + 1;
-$('#score').html('score: ' + score)
+		//displaying the random # in the console
+		console.log('Total Score: ' + totalScore)
 
+		$('#score').html(totalScore)
 
-} //end of reset
-
-
-//not generating new number on reset
-//not adding wins/losses
-//
-$('#gem1').click(function () {
-    console.log('gem1: ' + gem1);
-    var newScore = score+= gem1;
-    //displaying the random # in the console
-    console.log('New Score: ' + newScore)
-    $('#score').html(newScore)
-
-    if (newScore === randomNumber) {
-        wins++;
-        $('#wins').html('wins: ' + wins);
-        reset();
-       } else if (newScore > randomNumber){
-           losses++;
-           $('#loss').html('losses: ' + losses);
-           reset();
-       }
-}) 
-    
-    
- $('#gem2').click(function () {
-    var newScore = score+= gem2;
-    //displaying the random # in the console
-    console.log('gem2: ' + gem2);
-    $('#score').html(newScore)
-
-    if (newScore === randomNumber) {
-        wins++;
-        $('#wins').html('wins: ' + wins);
-        reset();
-       } else if (newScore > randomNumber){
-           losses++;
-           $('#loss').html('losses: ' + losses);
-           reset();
-       }
-
-   
-})
-
-$('#gem3').click(function () {
-    var newScore = score+= gem3;
-    //displaying the random # in the console
-    console.log('gem3: ' + gem3);
-    $('#score').html(newScore)
-    
-    if (newScore === randomNumber) {
-        wins++;
-        $('#wins').html('wins: ' + wins);
-        reset();
-       } else if (newScore > randomNumber){
-           losses++;
-           $('#loss').html('losses: ' + losses);
-           reset();
-       }
-
-    
-})
-
-$('#gem4').click(function () {
-    var newScore = score+= gem4;
-    //displaying the random # in the console
-    console.log('gem4: ' + gem4);
-    $('#score').html(newScore)
-    
-    if (newScore === randomNumber) {
-        wins++;
-        $('#wins').html('wins: ' + wins);
-        reset();
-       } else if (newScore > randomNumber){
-           losses++;
-           $('#loss').html('losses: ' + losses);
-           reset();
-       }
-   
-})
+		if (totalScore === randomNumber) {
+			wins++;
+			$('#wins').html('wins: ' + wins);
+			reset();
+		} else if (totalScore > randomNumber) {
+			losses += losses;
+			$('#loss').html('losses: ' + losses);
+			reset();
+		}
+	})
 
 
- $('#randomNum').text(randomNumber);
+	$('#gem2').click(function () {
+		var totalScore = score += gem2;
 
-}); //end of everything
+		//displaying the random # in the console
+		console.log('gem2: ' + gem2);
 
+		$('#score').html(totalScore)
+		console.log('Total Score: ' + totalScore)
+
+		if (totalScore === randomNumber) {
+			wins++;
+			$('#wins').html('wins: ' + wins);
+			reset();
+		} else if (totalScore > randomNumber) {
+			losses += losses;
+			$('#loss').html('losses: ' + losses);
+			reset();
+		}
+	})
+
+	$('#gem3').click(function () {
+		var totalScore = score += gem3;
+
+		//displaying the random # in the console\
+		console.log('gem3: ' + gem3);
+		console.log('Total Score: ' + totalScore)
+
+		$('#score').html(totalScore)
+
+		if (totalScore === randomNumber) {
+			wins += wins;
+			$('#wins').html('wins: ' + wins);
+			reset();
+		} else if (totalScore > randomNumber) {
+			losses += losses;
+			$('#loss').html('losses: ' + losses);
+			reset();
+		}
+	})
+
+	$('#gem4').click(function () {
+		var totalScore = score += gem4;
+
+		//displaying the random # in the console
+		console.log('gem4: ' + gem4);
+
+		$('#score').html(totalScore)
+		console.log('Total Score: ' + totalScore)
+
+		if (totalScore === randomNumber) {
+			wins += wins;
+			$('#wins').html('wins: ' + wins);
+			reset();
+		} else if (totalScore > randomNumber) {
+			losses += losses;
+			$('#loss').html('losses: ' + losses);
+			reset();
+		}
+	})
+
+	$('#randomNum').text(randomNumber);
+	console.log("wins " + wins)
+	console.log("losses " + losses)
+}); 
